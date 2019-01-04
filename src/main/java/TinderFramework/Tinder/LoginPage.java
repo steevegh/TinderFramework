4d2c14fcee495d092aeafde95a97d5776f701620
steevegh <steeve.ghallab@gmail.com>
2019-01-04 14:00:51 +0100

    test4

--- src/main/java/TinderFramework/Tinder/LoginPage.java
+++ src/main/java/TinderFramework/Tinder/LoginPage.java
@@ -1,9 +1,12 @@
 package TinderFramework.Tinder;
 
+import java.util.Iterator;
+import java.util.Set;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class LoginPage{
+public class LoginPage {
 
 	WebDriver driver;
 
@@ -15,12 +18,12 @@ public class LoginPage{
 		try {
 			Thread.sleep(4000);
 			driver.findElement(By.xpath("//div[@id='modal-manager']/div/div/div[2]/div/div[3]/div/button/span/span"))
-			.click();
+					.click();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	public String openPage() {
@@ -33,6 +36,25 @@ public class LoginPage{
 		}
 		String tinderText = driver.getCurrentUrl();
 		return tinderText;
+
+	}
+
+	public void LoginWithFacebookAccountGetStarted() throws InterruptedException {
+		String usernameEmailFB = "Test";
+		String passwordEmailFB = "Test";
+		Set<String> it = driver.getWindowHandles();
+		Iterator<String> iterator = it.iterator();
+		Thread.sleep(1000);
+		while (iterator.hasNext()) {
+			String childWindows = iterator.next();
+			if (!iterator.next().equalsIgnoreCase(childWindows))
+				;
+			driver.switchTo().window(childWindows);
+			Thread.sleep(1000);
+			
+
+		}
+		System.out.println("No page found");
 
 	}
 }
